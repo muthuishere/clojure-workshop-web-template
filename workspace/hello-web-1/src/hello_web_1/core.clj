a8ed666f4a74db945b1f249522a721d5ad5b1b7b
Muthukumaran <muthuishere@gmail.com>
2021-12-14 14:59:23 +0530

    Initial Web

--- workspace/hello-web-1/src/hello_web_1/core.clj
+++ workspace/hello-web-1/src/hello_web_1/core.clj
@@ -2,11 +2,26 @@
   (:require
     [compojure.core :refer :all]
     [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+    [ring.middleware.json :as middleware]
     [ring.adapter.jetty :refer :all]
     [compojure.route :as route]
     )
   )
+(defn insert-data [request]
+  (println request)
 
+
+  {
+   :status 200
+   :body {
+          :result (str "Created Successfully for" (get-in request [:body :name]))
+
+          }
+
+
+
+   }
+  )
 (defroutes app-routes
 
   (GET "/hello" request (fn [request]
@@ -17,17 +32,44 @@
   (GET "/hello/:name"  request (fn [request]
 
                                  (println request)
-                                 "Testing"
+                                 (str "Welcome " (get-in request [:params :name]))
+                                 )
 
-                                 ; Exercise
-                                 ; Get the name and print Welcome {name}
-                                 ))
+                               )
+
+
+           (POST "/insertdata" request  (fn [request]
+
+                                          (insert-data request)
+                                          )
+                                        )
+
+
+
 
     (route/not-found "Url Not found")
   )
 
+; Home Assignment
+; A Post Request /add
+;{
+; "a":23,
+; "b":45
+; }
 
-(def app (wrap-defaults  app-routes api-defaults))
+; Result Should be
+;
+;{
+; "result" : 68
+; }
+(def app (wrap-defaults
+
+
+           (middleware/wrap-json-response
+             (middleware/wrap-json-body app-routes {:keywords? true})
+             )
+
+           api-defaults))
 
 
 (defn -main [& args]
